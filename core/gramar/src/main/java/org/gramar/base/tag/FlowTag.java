ab75258f3bae19f6f03d36d8ee556f422482ea54
chrisgerken <chris.gerken@gerkenip.com>
2015-08-30 20:35:09 -0500

    flow tags; fixes; doc

--- core/gramar/src/main/java/org/gramar/base/tag/FlowTag.java
+++ core/gramar/src/main/java/org/gramar/base/tag/FlowTag.java
@@ -1,5 +1,7 @@
 package org.gramar.base.tag;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 import org.gramar.IGramarContext;
@@ -37,8 +39,53 @@ public class FlowTag extends TagHandler implements ITagHandler {
 		String toFlow = merged.substring(userRegion.getInitialCodeStart(), userRegion.getInitialCodeEnd());
 		after  = merged.substring(userRegion.getInitialCodeEnd(), userRegion.getUserRegionEnd());
 
+		String lines[] = split(toFlow);
+		
+		try {
+			for (int line = 0; line < lines.length; line++) {
+				stream.append(before);
+				stream.append(lines[line]);
+				stream.append(after);
+				if (line < (lines.length-1)) {
+					stream.append("\n");
+				}
+			}
+		} catch (IOException e) {
+			context.error(e);
+		}
 		
 		
+	}
+
+	private String[] split(String toFlow) {
+		ArrayList<String> lines = new ArrayList<String>();
+		StringTokenizer st = new StringTokenizer(toFlow, "\n");
+		while (st.hasMoreTokens()) {
+			String buf = st.nextToken();
+			if (buf.length() == 0) {
+				lines.add("");
+			} else {
+				while (buf.length() > 0) {
+					if (buf.length() <= width) {
+						lines.add(buf);
+						buf = "";
+					} else {
+						int index = width - 1;
+						while ((index > -1) && (!Character.isWhitespace(buf.charAt(index)))) {
+							index--;
+						}
+						if (index < 0) {
+							index = width - 1;
+						}
+						lines.add(buf.substring(0,index+1));
+						buf = buf.substring(index+1);
+					}
+				}
+			}
+		}
+		String result[] = new String[lines.size()];
+		lines.toArray(result);
+		return result;
 	}
 
 	@Override
