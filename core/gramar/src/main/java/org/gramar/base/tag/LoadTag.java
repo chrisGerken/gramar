75eb0a5bee10afb5d87ce4179a20870bb5ee7494
chrisgerken <chris.gerken@gerkenip.com>
2015-09-27 08:03:49 -0500

    load tag

--- core/gramar/src/main/java/org/gramar/base/tag/LoadTag.java
+++ core/gramar/src/main/java/org/gramar/base/tag/LoadTag.java
@@ -1,11 +1,17 @@
 package org.gramar.base.tag;
 
+import java.io.InputStream;
+
 import org.gramar.IGramarContext;
+import org.gramar.IGramarPlatform;
 import org.gramar.IModel;
+import org.gramar.IModelAdaptor;
 import org.gramar.ITagHandler;
 import org.gramar.model.XmlModel;
 import org.gramar.resource.MergeStream;
 import org.gramar.tag.TagHandler;
+import org.gramar.util.GramarHelper;
+import org.w3c.dom.Document;
 
 public class LoadTag extends TagHandler implements ITagHandler {
 
@@ -17,15 +23,46 @@ public class LoadTag extends TagHandler implements ITagHandler {
 	public void mergeTo(MergeStream stream, IGramarContext context) {
 
 		try {
+
+			String url = getStringAttribute("url", context);
+			String urlContext = getStringAttribute("urlContext", context, "gramar");  //  One of "gramar" (default), workspace and "transform"
+			if (urlContext.equalsIgnoreCase("transform")) {
+				urlContext = "gramar";
+			}
 			String name = getStringAttribute("var", context);
 			String loader = getStringAttribute("loader", context, null);
 			String type = getStringAttribute("type", context, null);
 
-			MergeStream newStream = processChildren(context);
-			String content = newStream.toString();
+			String content = null;
+
+			if (urlContext.equalsIgnoreCase("gramar")) {
+				content = context.getGramar().getTemplateSource(url);
+			} else if (urlContext.equalsIgnoreCase("workspace")) {
+				InputStream is = context.getFileStore().getFileByteContent(url);
+				content = GramarHelper.asString(is);
+			} else {
+				context.error("Invalid urlContext for load tag: "+urlContext);
+				return;
+			}
 			
-			IModel inline = new XmlModel(content);
-			context.setVariable(name, inline.asDOM());
+			IModelAdaptor adaptor = null;
+			IGramarPlatform platform = context.getPlatform();
+			if (loader != null) {
+				try { adaptor = platform.getModelAdaptor(loader); } catch (Throwable t) { }
+			}
+			if ((adaptor == null) && (type != null)) {
+				try { adaptor = platform.getModelAdaptorFor(type); } catch (Throwable t) { }
+			}
+			
+			Document doc = null;
+			if (adaptor != null) {
+				doc = adaptor.asDocument(content);
+			} else {
+				doc = new XmlModel(content).asDOM();
+			}
+			
+			context.setVariable(name, doc);
+			
 		} catch (Exception e) {
 			context.error(e);
 		}
@@ -34,7 +71,7 @@ public class LoadTag extends TagHandler implements ITagHandler {
 
 	@Override
 	public String getTagName() {
-		return "loadContent";
+		return "load";
 	}
 
 }
