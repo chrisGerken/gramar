8c40d467e958721a1c0f53e2bd9d5ef2f8edf0cf
chrisgerken <chris.gerken@gerkenip.com>
2015-09-30 09:21:38 -0500

    copyElement

---
+++ core/gramar/src/main/java/org/gramar/base/tag/CopyElementTag.java
@@ -0,0 +1,44 @@
+package org.gramar.base.tag;
+
+import org.gramar.IGramarContext;
+import org.gramar.ITagHandler;
+import org.gramar.resource.MergeStream;
+import org.gramar.tag.TagHandler;
+import org.w3c.dom.Node;
+
+public class CopyElementTag extends TagHandler implements ITagHandler {
+
+	public CopyElementTag() {
+
+	}
+
+	@Override
+	public void mergeTo(MergeStream stream, IGramarContext context) {
+		
+		try {
+
+			Node node = getNodeAttribute("select", context);
+			Node parent = getNodeAttribute("toSelect", context);
+			String name = getStringAttribute("name", context);
+			String var = getStringAttribute("var", context, null);
+			Boolean deep = getBooleanAttribute("recursive", context, true);
+
+			Node copy = node.cloneNode(deep);
+			parent.appendChild(copy);
+
+			if (var != null) {
+				context.setVariable(var, copy);
+			}
+
+		} catch (Exception e) {
+			context.error(e);
+		}
+
+	}
+
+	@Override
+	public String getTagName() {
+		return "copyElement";
+	}
+
+}
